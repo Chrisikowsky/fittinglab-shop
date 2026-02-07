002b3473b5e99b10df9d1cfb40eee2833654be5d
Chrisikowsky <chriss.schulte88@googlemail.com>
2026-02-07 15:44:56 +0100

    fix(backend): conditionally load redis modules to support build time

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -2,22 +2,11 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
-module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  },
-  admin: {
-    path: "/dashboard",
-  },
-  modules: [
+
+const modules = [];
+
+if (process.env.REDIS_URL) {
+  modules.push(
     {
       resolve: "@medusajs/event-bus-redis",
       options: {
@@ -37,6 +26,24 @@ module.exports = defineConfig({
       options: {
         redisUrl: process.env.REDIS_URL,
       },
-    },
-  ],
+    }
+  );
+}
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  admin: {
+    path: "/dashboard",
+  },
+  modules,
 })
